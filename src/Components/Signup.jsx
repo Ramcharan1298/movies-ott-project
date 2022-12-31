78a715438313214a6bd8291ad46d9b77c2a8f32d
DivyaRayadurgam143 <114801687+DivyaRayadurgam143@users.noreply.github.com>
2022-12-31 13:13:36 +0530

    modified signup

---
+++ src/Components/Signup.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+export default class Signupform extends Component {
+  render() {
+    return (
+      <div className="box">
+        <div className="form">
+          <h2>SIGN Up</h2>
+
+          <div className="inputBox">
+            <input
+              type="email"
+              placeholder="Youremail@gmail.com"
+              required="required"
+            />
+            <span>EMAIL:</span>
+            <i></i>
+          </div>
+
+          <div className="inputBox">
+            <input
+              type="text"
+              placeholder="********"
+             required="required"
+            />
+            <span>PASSWORD:</span>
+            <i></i>
+          </div>
+
+          <div className="inputBox">
+            <input
+              type="text"
+              placeholder="********"
+             required="required"
+            />
+            <span>Confirm-PASSWORD:</span>
+            <i></i>
+          </div>
+
+            <div className="links">
+                <a href="#">Forgot Password</a>
+                <a href="#">Sign Up</a>
+            </div>
+            <input type="submit" value="signup"/>
+          </div>
+        </div>
+    );
+  }
+}
